7f6f9b90d0783e47ff0f45a4dd4be4b208347e2b
unknown <brenomoraes97@yahoo.com>
2023-01-18 03:33:27 -0300

    feat: setup

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import { ActivityIndicator, View } from "react-native";
+
+export const Loanding =() =>{
+
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems:'center', backgroundColor:'09090A'}}>
+            <ActivityIndicator color='#7C3AED'/>
+
+        </View>
+    )
+};
