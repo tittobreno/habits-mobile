b2db86e5cdd9ee9407502a9baffb6f9a653ed2b7
unknown <brenomoraes97@yahoo.com>
2023-01-22 02:42:22 -0300

    feat: created checkbox

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,6 +1,28 @@
 import { View, ScrollView, Text, TextInput } from "react-native";
 import BackButton from "../components/BackButton";
+import CheckBox from "../components/CheckBox";
+import { useState } from "react";
+const availableWeekDays = [
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+  "Domingo",
+];
 const New = () => {
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  const handleToggleWeekDay = (weekDayIndex: number) => {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays((prevState) =>
+        prevState.filter((weekDay) => weekDay !== weekDayIndex)
+      );
+    } else {
+      setWeekDays((prevState) => [...prevState, weekDayIndex]);
+    }
+  };
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <ScrollView showsVerticalScrollIndicator={false}>
@@ -14,7 +36,18 @@ const New = () => {
           Qual o seu comprometimento?
         </Text>
 
-        <TextInput className="h-12 pl-4 mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600" />
+        <TextInput className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600" />
+        <Text className="font-semibold mt-4 mb-3 text-white text-base">
+          Qual a recorrência?
+        </Text>
+        {availableWeekDays.map((weekDay, i) => (
+          <CheckBox
+            key={weekDay}
+            title={weekDay}
+            checked={weekDays.includes(i)}
+            onPress={() => handleToggleWeekDay(i)}
+          />
+        ))}
       </ScrollView>
     </View>
   );
