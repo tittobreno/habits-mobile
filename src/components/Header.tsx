1e59934ebcbfd211df599b5541ea6c4781fd9115
unknown <brenomoraes97@yahoo.com>
2023-01-22 01:31:29 -0300

    feat: created route system

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,9 @@ import { View, TouchableOpacity, Text } from "react-native";
 import Logo from "../assets/logo.svg";
 import { Feather } from "@expo/vector-icons";
 import colors from "tailwindcss/colors";
+import { useNavigation } from "@react-navigation/native";
 const Header = () => {
+  const { navigate } = useNavigation();
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
@@ -10,6 +12,7 @@ const Header = () => {
       <TouchableOpacity
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
         activeOpacity={0.7}
+        onPress={() => navigate("new")}
       >
         <Feather name="plus" color={colors.violet[500]} size={20} />
 
